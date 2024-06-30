87b392c49f6692f804d4e294204f67b9bbd403de
MauricioPMenezes <m.pereira.menezes@gmail.com>
2024-06-30 18:27:33 -0300

    Get Products paged

--- mpcommerce/src/main/java/com/devsuperior/mpcommerce/Services/ProductService.java
+++ mpcommerce/src/main/java/com/devsuperior/mpcommerce/Services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.mpcommerce.dto.ProductDTO;
 import com.devsuperior.mpcommerce.entities.Product;
 import com.devsuperior.mpcommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,15 +20,15 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
-
         ProductDTO dto = new ProductDTO(product);
-
         return dto;
-
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+         Page<Product> result= repository.findAll(pageable);
+         return result.map(x->new ProductDTO(x));
 
     }
-
 }
--- mpcommerce/src/main/java/com/devsuperior/mpcommerce/controllers/ProductController.java
+++ mpcommerce/src/main/java/com/devsuperior/mpcommerce/controllers/ProductController.java
@@ -2,15 +2,15 @@ package com.devsuperior.mpcommerce.controllers;
 
 import com.devsuperior.mpcommerce.Services.ProductService;
 import com.devsuperior.mpcommerce.dto.ProductDTO;
-import com.devsuperior.mpcommerce.entities.Product;
-import com.devsuperior.mpcommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,9 +21,13 @@ public class ProductController {
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable  Long id){
-
         ProductDTO dto=service.findById(id);
         return service.findById(id);
+
+    }
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
 
     }
 
